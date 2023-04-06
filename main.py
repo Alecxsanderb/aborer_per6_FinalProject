753dfd498729ba2af997a4910307134c7d294358
Alecxsanderb <adborer65@gmail.com>
2023-04-06 14:12:17 -0700

    polished up game

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ my goal is to create a mob that chases the player. game will be like tag and sco
 
 # import libs
 import pygame as pg
-import random
 import os
 # import settings 
 from settings import *
@@ -26,9 +25,10 @@ class Game:
         self.clock = pg.time.Clock()
         self.running = True
 
+    # start new game, create all sprites
     def new(self):
-        # start new game
-        self.menu = True
+        # began creating main menu and option for two players, but did not get to finish
+        self.menu = False
         self.end = False
         self.score = 0
         self.time = 0
@@ -40,12 +40,14 @@ class Game:
         self.ground = Platforms(WIDTH - 400, 30, 200, HEIGHT - 200, GREEN, "normal")
         self.enemy = Mob(self, self.player1, 75, 75, RED)
         self.enemies.add(self.enemy)
-        self.mc.add(self.player1)
         self.all_sprites.add(self.enemy)
+        self.mc.add(self.player1)
         self.all_sprites.add(self.player1)
         self.all_sprites.add(self.ground)
         self.platforms.add(self.ground)
+        # where the game runs
         self.run() 
+
     def run(self):
         self.playing = True
         while self.playing:
@@ -54,18 +56,25 @@ class Game:
             self.update()
             self.draw()
 
+    # does actions if buttons are pressed
     def events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
                 self.running = False
+            # this section resets the game if R is pressed
+            # Note: I could not figure out how to reset the timer
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_r:
                     self.player1.living = False
                     self.new()
+    
+    # updates game
     def update(self):
+        # draws sprites
         self.all_sprites.update()
+        # this section essentially makes platforms solid for the player, has stuff set up for variants but they aren't used
         if self.player1.vel.y > 0:
             hits = pg.sprite.spritecollide(self.player1, self.platforms, False)
             if hits:
@@ -75,15 +84,19 @@ class Game:
                 if hits[0].variant == "bouncy":
                     self.player1.pos.y = hits[0].rect.top
                     self.player1.vel.y = -20
+        # this section gives the mob the ability to collide with platforms
         if self.enemy.vel.y > 0:
             landing = pg.sprite.spritecollide(self.enemy, self.platforms, False)
             if landing:
                 if landing[0]:
                     self.enemy.pos.y = landing[0].rect.top
                     self.enemy.vel.y = 0
+        # this section ends the game if the player falls off the screen
         if not self.player1.living:
             self.end = True
+        self.difficulty()
 
+    # draws the different screens
     def draw(self):
         if self.menu and not self.end:
             self.main_screen()
@@ -95,6 +108,8 @@ class Game:
             self.timer()
             self.end_screen()
         pg.display.flip()
+    
+    # create text
     def draw_text(self, text, size, color, x, y):
         font_name = pg.font.match_font('arial')
         font = pg.font.Font(font_name, size)
@@ -102,29 +117,41 @@ class Game:
         text_rect = text_surface.get_rect()
         text_rect.midtop = (x,y)
         self.screen.blit(text_surface, text_rect)
+    
+    # unused method to get mouse position
     def get_mouse_now(self):
         x,y = pg.mouse.get_pos()
         return (x,y)
+    
+    # how the timer is created
     def timer(self):
+        # while the game is running
         if self.player1.living:
             self.time = pg.time.get_ticks()
             self.draw_text("Time: " + str(self.time/1000), 50, WHITE, WIDTH/2, 30)
+        # after the game ends
         else:
             self.draw_text("Time: " + str(self.time/1000), 50, WHITE, WIDTH/2, HEIGHT/2)
+
+    # main screen that I have not gotten working yet
     def main_screen(self):
         self.screen.fill(TEAL)
         self.draw_text("Menu", 150, WHITE, WIDTH/2, HEIGHT/2 - 150)
-        self.draw_text("Press 1 for 1 player", 75, WHITE, WIDTH/2 - 200, HEIGHT/2 + 100)
-        self.draw_text("Press 2 for 2 players", 75, WHITE, WIDTH/2 + 200, HEIGHT/2 + 100)
+        self.draw_text("Press 1 for 1 player", 30, WHITE, WIDTH/2 - 200, HEIGHT/2 + 100)
+        self.draw_text("Press 2 for 2 players", 30, WHITE, WIDTH/2 + 200, HEIGHT/2 + 100)
+
+    # end screen
     def end_screen(self):
         if not self.player1.living:
             self.screen.fill(BLACK)
             self.draw_text("Game Over", 200, WHITE, WIDTH/2, HEIGHT/2 - 250)
             self.timer()
             self.draw_text("Press R to restart", 50, WHITE, WIDTH/2, HEIGHT/2 + 150)
+
+    # method that increases the speen of the enemy as time goes on
     def difficulty(self):
         if self.player1.living:
-            self.enemy.enemyspeed *= 1.0005
+            self.enemy.enemyspeed *= 1.001
         if not self.player1.living:
             self.enemy.enemyspeed = 0
 
--- sprites.py
+++ sprites.py
@@ -6,9 +6,8 @@ from random import randint
 
 vec = pg.math.Vector2
 
+
 # player class
-
-
 class Player(Sprite):
     def __init__(self, game, playernumber):
         Sprite.__init__(self)
@@ -25,7 +24,10 @@ class Player(Sprite):
         self.canjump = False 
         self.living = True
         self.number = playernumber
+
+    # inputs for players, is capable of distinguishing between two players 
     def input(self):
+        # player 1 controls
         if self.number == "p1":
             keystate = pg.key.get_pressed()
             if keystate[pg.K_a]:
@@ -34,6 +36,7 @@ class Player(Sprite):
                 self.acc.x = +PLAYER_ACC
             if keystate[pg.K_w]:
                 self.jump()
+        # player 2 controls
         if self.number == "p2":
             keystate = pg.key.get_pressed()
             if keystate[pg.K_LEFT]:
@@ -42,13 +45,16 @@ class Player(Sprite):
                 self.acc.x = +PLAYER_ACC
             if keystate[pg.K_UP]:
                 self.jump()
+    
+    # jump method 
     def jump(self):
         self.rect.x += 1
         hits = pg.sprite.spritecollide(self, self.game.platforms, False)
         self.rect.x -= 1
         if hits:
             self.vel.y = -PLAYER_JUMP
-    # method to keep it on screen
+    
+    # method to keep it on screen, or end the game if the player falls off
     def inbounds(self):
         if self.pos.x > WIDTH - 25:
             # print("I am off the right side of the screen")
@@ -65,12 +71,15 @@ class Player(Sprite):
             # print("I am off the right side of the screen")
             self.pos.y = 25
             self.vel.y *= 0
+        
+    # method for creating a collision with mobs
     def mob_collide(self):
         hits = pg.sprite.spritecollide(self, self.game.enemies, False)
         if hits:
             self.vel.x *= -1.5
             self.vel.y += self.vel.x/2
 
+    # update and physics
     def update(self):
         self.mob_collide()
         self.inbounds()
@@ -81,8 +90,12 @@ class Player(Sprite):
         self.pos += self.vel + 0.5 * self.acc
         self.rect.midbottom = self.pos
 
+
+
+# mob class
 class Mob(Sprite):
     def __init__(self, game, player, width, height, color):
+        # properties of mob
         Sprite.__init__(self)
         self.player = player
         self.game = game
@@ -96,10 +109,11 @@ class Mob(Sprite):
         self.pos = vec(randint(200, 1100), randint(0, 800))
         self.vel = vec( 3*randint(-5,5)/randint(1,20), 3*randint(-5,5)/randint(1,20))
         self.acc = vec(0,0)
-        self.cofric = 0.1
+        self.cofric = 0.2
         self.canjump = True
         self.enemyspeed = .1
-    # method to keep it on screen
+
+    # method to keep it on screen and get it back on the platform if it falls off
     def inbounds(self):
         if self.pos.x > WIDTH - 40:
             # print("I am off the right side of the screen")
@@ -118,21 +132,27 @@ class Mob(Sprite):
             self.pos.y = 25
             self.vel.y *= -1
 
+    # method for creating collision with player
     def player_collide(self):
         hits = pg.sprite.spritecollide(self, self.game.mc, False)
         if hits:
             self.vel.x += self.player.vel.x/3
             self.vel.y += self.vel.x/2
 
+    # how mob chases player
     def chase(self):
+        # if the player is right of the mob, move right
         if self.player.pos.x > self.pos.x:
             self.vel.x += self.enemyspeed
+        # if the player is to the left of the mob, move left
         if self.player.pos.x < self.pos.x:
             self.vel.x -= self.enemyspeed
+        # randomly jump
         r = randint(0,75)
         if r == 1:
             self.jump()
         
+    # jumps if in contact with ground
     def jump(self):
         self.rect.x += 1
         hits = pg.sprite.spritecollide(self, self.game.platforms, False)
@@ -140,6 +160,7 @@ class Mob(Sprite):
         if hits:
             self.vel.y = -MOB_JUMP
 
+    # mob behavior
     def behavior(self):
         # print(self.vel)
         self.inbounds()
@@ -147,6 +168,8 @@ class Mob(Sprite):
         self.player_collide()
         self.pos += self.vel
         self.rect.center = self.pos
+
+    # update and physics
     def update(self):
         self.behavior()
         self.acc = vec(0, MOB_GRAV)
@@ -155,6 +178,9 @@ class Mob(Sprite):
         self.pos += self.vel + 0.5 * self.acc
         self.rect.midbottom = self.pos
 
+
+
+# platform class
 class Platforms(Sprite):
     def __init__(self, width, height, x, y, color, variant):
         Sprite.__init__(self)
